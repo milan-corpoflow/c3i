0c880832929ec07ca36753312ce726cc67c75d75
Milan van As <milan@corpoflow.nl>
2018-10-12 14:34:28 +0200

    Fixes production build of frontend

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,7 @@ module.exports = (env) => {
 
         // Optimization
         optimization: {
+            concatenateModules: false,
             runtimeChunk: {
                 name: 'commons',
             },
